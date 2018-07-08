33a95fb08be9c8176eee956de3ddbf033e8d22db
Michal Zalewski <michal.zalewski@student.put.poznan.plgit commit -m Initial>
2018-07-08 14:40:47 +0200

    Tests

--- Tests.EndToEnd/Controllers/UsersControllerTests.cs
+++ Tests.EndToEnd/Controllers/UsersControllerTests.cs
@@ -1,12 +1,15 @@
 ﻿using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Api;
+using Infrastructure.Commands.Users;
 using Infrastructure.DTO;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.WebSockets.Internal;
 using Newtonsoft.Json;
 using NUnit.Framework;
 
@@ -28,11 +31,7 @@ namespace Tests.EndToEnd.Controllers
         public async Task GivenValidEmailUserShouldExist()
         {
             var email = "user1@email.com";
-            var response = await _client.GetAsync($"users/{email}");
-            response.EnsureSuccessStatusCode();
-
-            var responseString = await response.Content.ReadAsStringAsync();
-            var user = JsonConvert.DeserializeObject<UserDTO>(responseString);
+            var user = await GetUserAsync(email);
 
             Assert.AreEqual(user.Email, email);
         }
@@ -42,12 +41,41 @@ namespace Tests.EndToEnd.Controllers
         {
             var email = "user1000@email.com";
             var response = await _client.GetAsync($"users/{email}");
-            response.EnsureSuccessStatusCode();
-
-            var responseString = await response.Content.ReadAsStringAsync();
-            var user = JsonConvert.DeserializeObject<UserDTO>(responseString);
 
             Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Test]
+        public async Task GivenUniqueEmailUserShouldBeCreated()
+        {
+            var request = new CreateUser
+            {
+                Email = "test@email.com",
+                Username = "test",
+                Password = "secret"
+            };
+            var payload = GetPayload(request);
+            var response = await _client.PostAsync("users", payload);
+
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+            Assert.AreEqual(response.Headers.Location.ToString(), $"users/{request.Email}");
+
+            var user = await GetUserAsync(request.Email);
+            Assert.AreEqual(user.Email, request.Email);
+        }
+
+        private async Task<UserDTO> GetUserAsync(string email)
+        {
+            var response = await _client.GetAsync($"users/{email}");
+            var responseString = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<UserDTO>(responseString);
+        }
+
+        private static StringContent GetPayload(object data)
+        {
+            var json = JsonConvert.SerializeObject(data);
+
+            return new StringContent(json, Encoding.UTF8, "application/json");
         }
     }
 }
